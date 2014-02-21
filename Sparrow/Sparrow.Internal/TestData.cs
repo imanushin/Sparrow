b46ae7e59b3eda2bde1ebdaa3b57537129f8f0c8
Igor Manushin <igor.manushin@deltacorvi.com>
2014-02-22 01:32:09 +0400

    +internal library

---
+++ Sparrow/Sparrow.Internal/TestData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparrow.Internal
+{
+    public sealed class TestData
+    {
+        public TestData(IEnumerable<SingleCommand> commands)
+        {
+            Commands = commands.ToList().AsReadOnly();
+        }
+
+        public IReadOnlyList<SingleCommand> Commands
+        {
+            get;
+            private set;
+        }
+    }
+}
