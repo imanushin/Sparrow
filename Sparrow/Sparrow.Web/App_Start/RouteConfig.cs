1b91eead56d5b13c75f6b45e39c128b4fcb64cd4
Igor Manushin <igor.manushin@deltacorvi.com>
2014-02-22 00:28:40 +0400

    +default redirect

--- Sparrow/Sparrow.Web/App_Start/RouteConfig.cs
+++ Sparrow/Sparrow.Web/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.UI.WebControls;
 
 namespace Sparrow.Web
 {
@@ -12,16 +13,24 @@ namespace Sparrow.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            
+
+            var viewDefaults = new
+            {
+                controller = "Default",
+                action = "View",
+                testIdentity = ""
+            };
+
+            routes.MapRoute(
+                name: "Empty",
+                url: "",
+                defaults: viewDefaults
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{action}/{testIdentity}",
-                defaults: new
-                {
-                    controller = "Default",
-                    action = "View",
-                    id = UrlParameter.Optional
-                }
+                defaults: viewDefaults
             );
         }
     }
